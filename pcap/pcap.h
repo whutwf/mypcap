960ae9ce796cb1050fc530740f56bed8e66fcd18
whutwf <whutwf@outlook.com>
2017-08-23 11:38:49 +0800

    add net struct for parse pcap file

--- pcap/pcap.h
+++ pcap/pcap.h
@@ -23,13 +23,14 @@ extern "C" {
 #include <errno.h>
 #include <sys/mman.h>
 #include <string.h>
-#include<sys/stat.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/if_ether.h>
 #include <net/if.h>
 #include <sys/ioctl.h>
-#include<netpacket/packet.h>
+#include <netpacket/packet.h>
+#include <stdint.h>
 
 #define TCPDUMP_MAGIC       0xa1b2c3d4
 #define PCAP_VERSION_MAJOR  2
@@ -39,6 +40,7 @@ extern "C" {
 #define MMAP_PAGE_SIZE	    65535
 #define RECV_BUFFER_SIZE    2048
 
+/**---------pcap file struct using--------- */
 struct pcap_file_header {
     unsigned int magic;
     unsigned short version_major;
@@ -56,45 +58,86 @@ struct pcap_packet_header {
     unsigned int len;		/* length this packet (off wire) */
 };
 
-unsigned int
-pcap_file_open(const char *pcap_file_name);
+/**---------net struct using--------- */
+#define ETH_ALEN    6
+struct ethernet_hdr {
+    unsigned char eth_dst[ETH_ALEN];
+    unsigned char eth_src[ETH_ALEN];
+    unsigned short eth_proto;
+};
+struct ipv4_hdr {
+    uint8_t ip_hl : 4;     /* header length */
+    uint8_t ip_v : 4;      /* version */
+    uint8_t ip_tos;            /* type of service */
+    uint16_t ip_len;
+    uint16_t ip_id;
+    uint16_t ip_off;           /* fragment offset field */
+    uint8_t ip_ttl;            /* time to live */
+    uint8_t ip_proto;
+    uint16_t ip_checksum;
+    struct in_addr ip_src;
+    struct in_addr ip_dst;
+};
 
-unsigned int
-pcap_write_file_header(unsigned int fd);
+struct tcp_hdr {
+    uint16_t src_port;
+    uint16_t dst_port;
+    uint32_t seq_num;
+    uint32_t ack_num;
+    uint8_t len;
+    uint8_t flags;
+    uint16_t window;
+    uint16_t checksum;
+    uint16_t urg_ptr;
+};
 
-unsigned int
-pcap_write_packet_header(unsigned int fd, unsigned int data_len);
+struct tcp_info {};
 
-unsigned int
-pcap_write_packet_data(unsigned int fd, const unsigned char *data, unsigned int data_len);
+struct udp_hdr {
+    uint16_t src_port;
+    uint16_t dst_port;
+    uint16_t len;
+    uint16_t checksum;
+};
 
-inline void
-pcap_file_close(unsigned int fd)
+struct udp_info {};
+
+struct icmp_hdr {
+    uint8_t type;
+    uint8_t code;
+    uint16_t checksum;
+    uint16_t id;
+    uint16_t sequence;
+};
+
+unsigned int pcap_file_open(const char *pcap_file_name);
+
+unsigned int pcap_write_file_header(unsigned int fd);
+
+unsigned int pcap_write_packet_header(unsigned int fd, unsigned int data_len);
+
+unsigned int pcap_write_packet_data(unsigned int fd, const unsigned char *data, unsigned int data_len);
+
+inline void pcap_file_close(unsigned int fd)
 {
     close(fd);
 }
 
-int
-ethernet_data_fetch(unsigned char *buffer, const char *eth_name, const char *pcap_file_name);
+// void pcap_file_hdr_print(struct pcap_hdr_s *)
 
-void
-buf_print(const char *buf, int n);
+int ethernet_data_fetch(unsigned char *buffer, const char *eth_name, const char *pcap_file_name);
 
-int
-p_mmap_file_addr(const char *path);
+void buf_print(const char *buf, int n);
 
-void
-p_mmap_write_file_header(int fd);
+int p_mmap_file_addr(const char *path);
 
-void
-p_mmap_write_packet_header(int fd, int data_len);
+void p_mmap_write_file_header(int fd);
 
-void
-p_mmap_write_packet_data(int fd, const unsigned char *data, int data_len);
+void p_mmap_write_packet_header(int fd, int data_len);
 
-void
-p_munmap(void *p_mmap, int page_length);
+void p_mmap_write_packet_data(int fd, const unsigned char *data, int data_len);
 
+void p_munmap(void *p_mmap, int page_length);
 
 #ifdef __cplusplus
 }
